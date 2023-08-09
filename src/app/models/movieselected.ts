1d67eee1a51b0b8df1becc87d134dd58138f72ae
Jhon Herrera <jjhonherrera113@gmail.com>
2023-08-08 23:55:39 -0500

    Se vincula moviesSelected

--- src/app/models/movieselected.ts
+++ src/app/models/movieselected.ts
@@ -1,13 +1,13 @@
 export class movieSelected {
-        id:string;
-        titulo:string;
-        anioLanzamiento:number;
-        descripcion:string;
-        tipo:string;
-        url:string;
-        genero:Array<string>;
-        duracion:string;
-        calificacion:number;
+        id: string;
+        titulo: string;
+        anioLanzamiento: number;
+        descripcion: string;
+        tipo: string;
+        url: string;
+        genero: Array<string>;
+        duracion: string;
+        calificacion: number;
         trailer: string
-      
+
 }
